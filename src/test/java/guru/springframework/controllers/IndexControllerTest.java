1267c12603bf8064f269936fd4ed0bf47b7b7afe
konfistador <m.karadjov@icloud.com>
2024-09-16 17:42:19 +0300

    MockMVC introduced to the IndexControllerTest

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -8,12 +8,17 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -28,6 +33,20 @@ public class IndexControllerTest {
     public void setUp( ) throws Exception {
         MockitoAnnotations.initMocks( this );
         sut = new IndexController( recipeService );
+    }
+
+    /**
+     * Great example on how to test Controllers.
+     * @throws Exception but it doesn't :)
+     */
+
+    @Test
+    public void testMockMVC( ) throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup( sut ).build();
+
+        mockMvc.perform( get("/") )
+                .andExpect( status().isOk() )
+                .andExpect( view().name( "index" ));
     }
 
     @Test
